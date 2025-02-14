e7fd406ca0dc3c980f5172b30082e1a1c6527936
alma-mata <mat23038@uvg.edu.gt>
2025-02-14 09:02:15 -0600

    Inicio de pre laboratorio
    Se comienza la programación del pre laboratorio. Este consta de un contador de 4 bits que incrementa cada 100ms

---
+++ Laboratorio2/Laboratorio2/main.asm
@@ -0,0 +1,14 @@
+;-----------------------------------------------
+; Universidad del Valle de Guatemala
+; IE2023: Programacion de Microcontroladores
+; Laboratorio2.asm
+
+; Autor: Alma Mata Ixcayau
+; Proyecto: Laboratorio 2
+; Descripcion: Contador binario de 4 bits que incrementa
+;	cada 100ms
+; Hardware: ATMEGA328P
+; Creado: 05/02/2024
+; Ultima modificacion: 06/02/2024
+;-----------------------------------------------
+
